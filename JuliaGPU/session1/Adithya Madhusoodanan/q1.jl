fe4e6fc9c4307796bf9ff512f029711ee09ad964
Adithya Madhusoodanan <44079850+adithyamadhusoodanan@users.noreply.github.com>
2019-05-15 22:47:25 +0530

    Create q1.jl

---
+++ JuliaGPU/session1/Adithya Madhusoodanan/q1.jl
@@ -0,0 +1,13 @@
+s=readline()
+s=parse(Int,s)
+while s!=1
+           if s%2==0
+           global s=s/2
+           println("$s")
+           
+           else 
+           global s=3s+1
+       println("$s")
+
+       end
+       end
